770df2fa9c2106abccaf62bd2afa8789daca0f7e
Reza <holo121@gmail.com>
2022-04-04 02:02:29 +0430

    first commit

--- src/index.js
+++ src/index.js
@@ -1,12 +1,17 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-const Element = React.createElement("div",
-    {
-        id: "main",
-        className: "container"
-    },
-    "Hello world"
-);
+const App = () => {
+    return (
+        React.createElement("div",
+            {
+                id: "main",
+                className: "container"
+            },
+            "Hello world"
+        )
+    )
+}
 
-ReactDOM.render(Element, document.getElementById("root"));
+
+ReactDOM.render(<App />, document.getElementById("root"));
